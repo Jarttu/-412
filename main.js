3b1cc40f7f795b3a90ab0eceefaea31653446ec3
Jarttu <167183980+Jarttu@users.noreply.github.com>
2025-01-28 13:05:07 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,81 @@
-let currentPlayer = 1;
-let p1TotalPoints = 0;
-let p2TotalPoints = 0;
-let p1PointsThisRound = 0;
-let p2PointsThisRound = 0;
 let rollsThisRound = 0;
 let goalPoints = 100;
-let roundWon = false;
+let playerCount = 2;
+let players = [];
+let currentPlayerI = 0;
 
-var restartBtn = document.getElementById("restartgame");
+var restartbtn = document.getElementById("restartbtn")
+var rollbtn = document.getElementById("rollbtn")
 
-restartBtn.setAttribute("disabled", " ");
+restartbtn.setAttribute("disabled", " ")
 
 document.addEventListener("DOMContentLoaded", function(){
-    const splashContainer = document.querySelector(".splash-container");
+    const splashContainer = document.getElementById("splash-container");
     const goalForm = document.getElementById("goalform");
-    const gameContent = document.getElementById("gamecontent");
     const goalInput = document.getElementById("goalinput");
+
+    const splash2 = document.getElementById("splash2");
+    const playerCountForm = document.getElementById("playercountform");
+    const playerCountInput = document.getElementById("playercountinput");
+    const nameForm = document.getElementById("nameform");
+    const playerNameInputs = document.getElementById("playernameinputs")
+
+    const gameContent = document.getElementById("gamecontent");
 
     function splashFadeOut(){
         splashContainer.classList.add('hidden');
+        setTimeout(function(){
+            splash2.classList.add('active');
+        }, 500);
+    }
 
+    function nameFormIn(){
+        playerCountForm.classList.add('hidden');
+        genNameInputs();
+        setTimeout(function(){
+            nameForm.classList.add('visible');
+            
+        }, 500);
+    }
+
+    function splash2FadeOut(){
+        splash2.classList.remove('active');
+        nameForm.classList.remove('visible');
         setTimeout(function(){
             gameContent.classList.add('visible');
-            displaydefault();
+            displayFirstDice()
+            updateCurrentPlayerDisplay()
+            displayPoints()
         }, 500);
-        
+    }
+
+    function genNameInputs(){
+        playerNameInputs.innerHTML = ""
+        for(let i = 1; i <= playerCount; i++){
+            const input = document.createElement("input")
+            input.type = "text"
+            input.placeholder = `Pelaajan ${i} nimi`
+            input.id = `player${i}name`
+            input.classList.add("player-name-input")
+            playerNameInputs.appendChild(input)
+        }
+        const startButton = document.createElement("button")
+        startButton.textContent = "Aloita peli"
+        startButton.type = "button"
+        startButton.onclick = startGame
+        startButton.classList.add('click')
+        playerNameInputs.appendChild(startButton)
+    }
+
+    function startGame(){
+        players = []
+        for(let i = 1; i <= playerCount; i++){
+            const playerName = document.getElementById(`player${i}name`).value.trim()
+            if (playerName){
+                players.push({id : `p${i}`, name : playerName, points: 0, currentroundpoints: 0})
+            }
+        }
+        splash2FadeOut()
     }
 
     goalForm.addEventListener("submit", function(event){
@@ -47,112 +98,105 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         }
     });
+
+    playerCountForm.addEventListener("submit", function(event){
+        event.preventDefault();
+
+        const playerCountValue = playerCountInput.value.trim();
+
+        if (playerCountValue == ""){
+            nameFormIn()
+        } else{
+            if (isNaN(playerCountValue)){
+                alert("Syötä numero")
+            } else if (playerCountValue < 2){
+                alert("Pelaajia on oltava vähintään 2")
+            } else if (playerCountValue > 10){
+                alert("Pelaajia voi olla enintään 10")
+            } else {
+                playerCount = parseInt(playerCountValue)
+                nameFormIn()
+            }
+        }
+    });
+
 });
 
+let diceImgcontainer1 = document.getElementById("dice-container-1")
 
+function nextPlayer(){
+    rollsThisRound = 0
+    currentPlayerI = (currentPlayerI + 1) % players.length;
+    updateCurrentPlayerDisplay()
+};
 
-function displaydefault(){
+function updateCurrentPlayerDisplay(){
+    const currentPlayer = players[currentPlayerI]
+    document.getElementById("playerturn").textContent = `Pelaajan vuoro: ${currentPlayer.name}`
+}
+
+function displayFirstDice(){
     const firstRandomNum = Math.floor(Math.random() * 6) + 1;
-    const firstDiceImage = "images/dice" + firstRandomNum + ".png";
-    document.querySelectorAll("img")[0].setAttribute("src", firstDiceImage);
-
-    document.getElementById("goaltext").textContent = `Tavoite: ${goalPoints}`
-    document.getElementById("tulos").textContent = "";
-    document.getElementById("goaltext").textContent = "Tavoite: 0";
-    document.getElementById("playerturn").textContent = `Pelaajan ${currentPlayer} vuoro`
-    document.getElementById("p1pointstotal").textContent = "P1 : 0";
-    document.getElementById("p2pointstotal").textContent = "P2 : 0";
-    document.getElementById("p1pointsround").textContent = "P1 : 0";
-    document.getElementById("p2pointsround").textContent = "P2 : 0";
-    document.getElementById("rolls").textContent = "Heitot : 0";
+    const firstDiceImage = "images/dice" +  firstRandomNum + ".png";
+    
+    diceImgcontainer1.src = firstDiceImage
 }
 
-function rolldice(){
-
-    rollsThisRound += 1;
-    const randomNum = Math.floor(Math.random() * 6) + 1;
-    const diceImage = "images/dice" + randomNum + ".png";
-    document.querySelectorAll("img")[0].setAttribute("src", diceImage);
-
-    if (randomNum == 1){
-        
-        if (currentPlayer == 1){
-            p1TotalPoints = 0;
-            p2TotalPoints -=1
-            
-        } else {
-            p2TotalPoints = 0;
-            p1TotalPoints -=1
-        
-        }
-        switchTurn();
-        
-    }
-
-    if (currentPlayer == 1){
-        p1PointsThisRound += randomNum;
-        p2PointsThisRound = 0;
-        if (randomNum == 1){
-            p1PointsThisRound -= 1
-        }
-    }
-    else{
-        p2PointsThisRound += randomNum;
-        p1PointsThisRound = 0;
-        if (randomNum == 1){
-            p2PointsThisRound -= 1
-        }
-        
-    }
-    
-    if (currentPlayer == 1){
-        p1TotalPoints += randomNum
-        
-    }
-    else {
-        p2TotalPoints += randomNum
-
-    }
-    
-    display()
-    
-}
-
-function switchTurn(){
-    rollsThisRound = 0;
-    p1PointsThisRound = 0;
-    p2PointsThisRound = 0;
-    currentPlayer = currentPlayer == 1 ? 2 : 1;
-    document.getElementById("playerturn").textContent = `Pelaajan ${currentPlayer} vuoro`
-    display()
+function displayPoints(){
+    document.getElementById("rolls").textContent = `Heitot: ${rollsThisRound}`
+    const pointsContainer = document.getElementById('points-container') || document.createElement("div")
+    pointsContainer.id = "points-container"
+    pointsContainer.innerHTML = ""
+    players.forEach(player => {
+        const playerPoints = document.createElement("div")
+        playerPoints.id = `points-${player.id}`
+        playerPoints.textContent = `${player.name}: ${player.points} pistettä (Pisteet tässä kierroksessa: ${player.currentroundpoints})`
+        pointsContainer.appendChild(playerPoints)
+    });
+    document.getElementById("gamecontent").appendChild(pointsContainer);
 }
 
 
 
-function display(){
-    if (p1TotalPoints >= goalPoints){
-        roundWon = true;
-        document.getElementById("result").textContent = "Pelaaja 1 voitti"
-        checkWin()
-    }
+function rollDice(){
+    rollsThisRound += 1
+    const randomNum1 = Math.floor(Math.random() * 6) + 1;
+    const diceImg1 = "images/dice" + randomNum1 + ".png";
+
+
+    diceImgcontainer1.src = diceImg1
     
-    if (p2TotalPoints >= goalPoints){
-        roundWon = true;
-        document.getElementById("result").textContent = "Pelaaja 2 voitti"
-        checkWin()
+    const currentPlayer = players[currentPlayerI]
+
+    
+    if(randomNum1 == 1){
+        currentPlayer.points -= currentPlayer.currentroundpoints
+        currentPlayer.currentroundpoints = 0
+        nextPlayer()
+    } else {
+        currentPlayer.currentroundpoints += randomNum1
+        currentPlayer.points += randomNum1
     }
-    document.getElementById("p1pointstotal").textContent = `P1 : ${p1TotalPoints}`
-    document.getElementById("p2pointstotal").textContent = `P2 : ${p2TotalPoints}`
-    document.getElementById("p1pointsround").textContent = `P1 : ${p1PointsThisRound}`
-    document.getElementById("p2pointsround").textContent = `P2 : ${p2PointsThisRound}`
-    document.getElementById("rolls").textContent = `Heitot : ${rollsThisRound}`
+
+    if(currentPlayer.points >= goalPoints){
+        
+        document.getElementById("result").textContent = `${currentPlayer.name} Voitti!`
+        buttons()
+        return
+    }
+    displayPoints()
+    
+};
+
+function stopTurn(){
+    const currentPlayer = players[currentPlayerI]
+    currentPlayer.currentroundpoints = 0
+    nextPlayer()
+    displayPoints()
 }
 
-function checkWin(){
-    restartBtn.removeAttribute("disabled");
-    
-}
-
-function restartGame(){
-    location.reload();
+function buttons(){
+    displayPoints()
+    rollbtn.setAttribute("disabled", " ")
+    restartbtn.removeAttribute("disabled")
 }
